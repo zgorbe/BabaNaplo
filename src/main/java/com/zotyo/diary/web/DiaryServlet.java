1163a55a9011c53faefa7c5987c5e0c7a3c649b2
Zoli <gzoleemail@gmail.com>
2011-09-16 08:25:47 +0200

    Getting the hostname

--- src/main/java/com/zotyo/diary/web/DiaryServlet.java
+++ src/main/java/com/zotyo/diary/web/DiaryServlet.java
@@ -1,6 +1,8 @@
 package com.zotyo.diary.web;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.GregorianCalendar;
 
 import javax.servlet.RequestDispatcher;
@@ -11,15 +13,28 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 
+import org.apache.log4j.Logger;
+
 import com.zotyo.diary.client.Diary;
 import com.zotyo.diary.client.DiaryImplService;
 
 public class DiaryServlet extends HttpServlet {
 	
+	private static Logger logger = Logger.getLogger(DiaryServlet.class); 
+	
 	private Diary diary;
 	private DatatypeFactory df;
 	
 	public void init() throws ServletException {
+		String hostname = "";
+		try {
+			InetAddress localhost = InetAddress.getLocalHost();
+			hostname = localhost.getHostName();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		logger.info("***hostname:" + hostname);
 		DiaryImplService diaryService = new DiaryImplService(); 
 		diary = diaryService.getDiaryImplPort();
 		
