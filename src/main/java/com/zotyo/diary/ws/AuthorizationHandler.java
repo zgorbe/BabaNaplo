2b9f93af913a5fda62c1e362db23a30617d467d8
zgorbe <gzoleemail@gmail.com>
2011-10-03 12:57:28 +0200

    AuthorizationHandler fixed, not tested

--- src/main/java/com/zotyo/diary/ws/AuthorizationHandler.java
+++ src/main/java/com/zotyo/diary/ws/AuthorizationHandler.java
@@ -3,7 +3,7 @@ package com.zotyo.diary.ws;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-
+import java.util.List;
 import javax.xml.namespace.QName;
 import javax.xml.soap.Node;
 import javax.xml.soap.SOAPBody;
@@ -30,9 +30,12 @@ public class AuthorizationHandler implements SOAPHandler<SOAPMessageContext> {
 				Iterator<Node> it = body.getChildElements();
 				Node operationNode = it.next();
 				if ("addDay".equals(operationNode.getLocalName()) || "addEvent".equals(operationNode.getLocalName())) {
-					/*Map requestHeaders = (Map)context.get(MessageContext.HTTP_REQUEST_HEADERS);
-					String keyword = (String) requestHeaders.get("keyword");*/
-					logger.info("Add method was called!");					
+					Map<String, List<String>> map = (Map<String, List<String>>) context.get(MessageContext.HTTP_REQUEST_HEADERS);
+					List<String> keyword = getHTTPHeader(map, "keyword");
+					if (keyword != null && keyword.size() > 0) {
+						String key = keyword.get(0);
+						logger.info("Add method was called! Keyword is: " + key);	
+					}
 				}
 			} catch(SOAPException se) {
 				logger.error(se);
@@ -54,4 +57,13 @@ public class AuthorizationHandler implements SOAPHandler<SOAPMessageContext> {
 	}
 
 	public void close(MessageContext context) {	}
+	
+	private List<String> getHTTPHeader(Map<String, List<String>> headers, String header) {
+		for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+			String name = entry.getKey();
+			if(name.equalsIgnoreCase(header))
+				return entry.getValue();
+		}
+		return null;
+	}
 }
