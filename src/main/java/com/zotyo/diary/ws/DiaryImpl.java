e054f2036ec41b23b8c425baf71519a5a57e38dd
t-zoltang <zoltan.gorbe@razorfish.com>
2013-05-15 14:40:08 +0200

    Trying to solve day issue 2

--- src/main/java/com/zotyo/diary/ws/DiaryImpl.java
+++ src/main/java/com/zotyo/diary/ws/DiaryImpl.java
@@ -11,6 +11,7 @@ import javax.jws.WebService;
 import javax.servlet.ServletContext;
 import javax.xml.ws.WebServiceContext;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
@@ -25,6 +26,8 @@ import com.zotyo.diary.util.DateUtil;
 @WebService(endpointInterface = "com.zotyo.diary.ws.Diary", wsdlLocation="WEB-INF/wsdl/DiaryImplService.wsdl")
 @HandlerChain(file = "handler-chain.xml")
 public class DiaryImpl /* extends SpringBeanAutowiringSupport */ implements Diary {
+	
+	private static Logger logger = Logger.getLogger(DiaryImpl.class);
 	
 	@Resource
 	private WebServiceContext context; 
@@ -45,7 +48,9 @@ public class DiaryImpl /* extends SpringBeanAutowiringSupport */ implements Diar
 	}
 	
 	public Day getDay(Date theDay) {
-		return diaryDAO.getDay(DateUtil.resetHMS(theDay));
+		Date date = DateUtil.resetHMS(theDay);
+		logger.info("Checking the date: " + date);
+		return diaryDAO.getDay(date);
 	}
 
 	public List<Event> getAllEvents() {
