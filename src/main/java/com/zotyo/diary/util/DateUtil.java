48c2263687e9ef67a957e1b33c6b4ee910a87f55
t-zoltang <zoltan.gorbe@razorfish.com>
2013-05-22 13:36:54 +0200

    Trying to solve day issue 4

--- src/main/java/com/zotyo/diary/util/DateUtil.java
+++ src/main/java/com/zotyo/diary/util/DateUtil.java
@@ -11,6 +11,7 @@ public class DateUtil {
 		cal.set(Calendar.HOUR_OF_DAY, 12);
 		cal.set(Calendar.MINUTE, 0);
 		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
 		return cal.getTime();
 	}
 }
