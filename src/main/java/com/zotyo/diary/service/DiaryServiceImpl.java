29f5ef12beb4c45ff40f7a6556cf7bb6fc2fc535
t-zoltang <zoltan.gorbe@razorfish.com>
2013-07-01 16:53:55 +0200

    Introducing Memcached in prod

--- src/main/java/com/zotyo/diary/service/DiaryServiceImpl.java
+++ src/main/java/com/zotyo/diary/service/DiaryServiceImpl.java
@@ -3,10 +3,10 @@ package com.zotyo.diary.service;
 import java.util.Date;
 import java.util.List;
 
-import net.spy.memcached.MemcachedClient;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.zotyo.diary.persistence.DiaryDAO;
@@ -19,30 +19,32 @@ public class DiaryServiceImpl implements DiaryService {
 	
 	private static Logger logger = Logger.getLogger(DiaryServiceImpl.class);
 	
-	private static final String DIARY_CACHE = "diarycache";
+	private static final String DIARY_CACHE = "diaryCache";
 	
 	@Autowired
 	private DiaryDAO diaryDAO;
 
-	//@Autowired
-	private MemcachedClient memcachedClient;
-	
+	@Cacheable(value = DIARY_CACHE, key="'events:' + #theDay.getTime()")
 	public List<Event> getEventsForADay(Date theDay) {
 		return diaryDAO.getEventsForADay(theDay);
 	}
-
+	
+	@Cacheable(value = DIARY_CACHE, key="'days:' + #year + '/' + #month")
 	public List<Day> getDaysForAMonth(int year, int month) {
 		return diaryDAO.getDaysForAMonth(year, month);
 	}
 
+	@Cacheable(value = DIARY_CACHE, key="'days'")
 	public List<Day> getAllDaysInDiary() {
 		return diaryDAO.getAllDaysInDiary();
 	}
-
+	
+	@Cacheable(value = DIARY_CACHE, key="'events'")
 	public List<Event> getAllEvents() {
 		return diaryDAO.getAllEvents();
 	}
 
+	@Cacheable(value = DIARY_CACHE, key="'days:' + #theDay.getTime()")
 	public Day getDay(Date theDay) {
 		return diaryDAO.getDay(theDay);
 	}
@@ -55,34 +57,42 @@ public class DiaryServiceImpl implements DiaryService {
 		return diaryDAO.searchTerms(term);
 	}
 	
-	public int addDay(Day day) {
-		return diaryDAO.addDay(day);
-	}
-
-	public int addEvent(Date theDay, Event event) {
-		return diaryDAO.addEvent(theDay, event);
-	}
-
+	@Cacheable(value = DIARY_CACHE, key="'days:count'")
 	public long getDayCount() {
 		return diaryDAO.getDayCount();
 	}
 
+	@Cacheable(value = DIARY_CACHE, key="'events:count'")
 	public long getEventCount() {
 		return diaryDAO.getEventCount();
 	}
 
+	@CacheEvict(value = DIARY_CACHE, allEntries = true)
+	public int addDay(Day day) {
+		return diaryDAO.addDay(day);
+	}
+
+	@CacheEvict(value = DIARY_CACHE, allEntries = true)
+	public int addEvent(Date theDay, Event event) {
+		return diaryDAO.addEvent(theDay, event);
+	}
+	
+	@CacheEvict(value = DIARY_CACHE, allEntries = true)
 	public int addWord(UniqueWord word) {
 		return diaryDAO.addWord(word);
 	}
 
+	@Cacheable(value = DIARY_CACHE, key="'words:' + #count")
 	public List<UniqueWord> getLatestWords(int count) {
 		return diaryDAO.getLatestWords(count);
 	}
-
+	
+	@Cacheable(value = DIARY_CACHE, key="'words'")
 	public List<UniqueWord> getAllWords() {
 		return diaryDAO.getAllWords();
 	}
 
+	@Cacheable(value = DIARY_CACHE, key="'words:count'")
 	public long getWordCount() {
 		return diaryDAO.getWordCount();
 	}
@@ -90,9 +100,4 @@ public class DiaryServiceImpl implements DiaryService {
 	public void setDiaryDAO(DiaryDAO diaryDAO) {
 		this.diaryDAO = diaryDAO;
 	}
-	
-	public void setMemcachedClient(MemcachedClient memcachedClient) {
-		this.memcachedClient = memcachedClient;
-	}
-
 }
