dee2d1ae493a635a973b0d4c4570b7e7927574a3
Zoli <zoli@ubuntu.ubuntu-domain>
2014-08-04 06:43:38 +0200

    Excluding dropdown from navbar on click handling

--- naplo-webapp/src/main/webapp/js/events.js
+++ naplo-webapp/src/main/webapp/js/events.js
@@ -205,7 +205,7 @@ var Events = (function() {
 
 			// init navbar collapse
 		    var $nav = $('.nav-collapse');
-			$nav.on('click', 'a', function() {
+			$nav.on('click', 'a:not(.dropdown-toggle)', function() {
 				$nav.collapse('hide');
 			});
 		},
